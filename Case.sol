6fe7b09501c098629a88e367fa7b15d90fbd82cc
Peshang <peseng88@gmail.comn>
2021-03-04 15:08:57 +0100

    added safeMath, addAlternatives(), Vote(). getCase

--- Case.sol
+++ Case.sol
@@ -1,17 +1,29 @@
-import "./Ownable.sol";
-pragma solidity 0.5.12;
+import "./Ownable1.sol";
+import "./safeMath.sol";
+pragma solidity 0.7.5;
+pragma abicoder v2;
+
 
 contract Cases is Ownable{
 
     struct Case {
       uint id;
       string title;
-      mapping(string => uint) alternativeCounter;
+      string option1;
+      string option2;
+      string option3;
+      
+      // alternativeCounter["Ja"] = 0
+      // mapping(string => uint) alternativeCounter;
       uint deadline;
       uint totalVotes;
       bool openForVoting;
+      string[] alternativesSting;
     }
-    uint caseNumber;
+    uint private caseNumber ;
+    
+    // 1. caseNumber, 2. alternativeText, 3. alternativesVotes. map _caseNumber to alternatives.
+    mapping(uint => mapping(string => uint)) caseAlternatives;
 
     event caseCreated(string title, bool openForVoting);
     event caseDeleted(string title, bool openForVoting, address deletedBy);
@@ -34,11 +46,11 @@ contract Cases is Ownable{
         newCase.title = _title;
         newCase.deadline = _deadline;
         newCase.totalVotes = _totalVotes;
-        caseNumber++;
+        caseNumber = SafeMath.add(caseNumber, 1);
 
-        insertCase(newCase, caseNumber);
+        insertCase(newCase, caseNumber, _totalVotes);
         creators.push(msg.sender);
-
+        
         assert(
             keccak256(
                 abi.encodePacked(
@@ -60,12 +72,19 @@ contract Cases is Ownable{
         );
         emit caseCreated(newCase.title, newCase.openForVoting);
     }
-    function insertCase(Case memory _newCase, uint _caseNumber) private {
+    function insertCase(Case memory _newCase, uint _caseNumber, uint _totalVotes) private {
         cases[_caseNumber] = _newCase;
+        addAlternatives(_caseNumber, "Ikke Stemt");
+        caseAlternatives[_caseNumber]["Ikke Stemt"] = _totalVotes;
     }
 
-    function getCase(uint _caseNumber) public view returns(string memory _title, uint _deadline, uint _totalVotes, bool _openForVoting){
-        return (cases[_caseNumber].title, cases[_caseNumber].deadline, cases[_caseNumber].totalVotes, cases[_caseNumber].openForVoting);
+    function getCase(uint _caseNumber) public view returns(string memory _title, uint _deadline, uint _totalVotes, bool _openForVoting, string[] memory _votes, uint[10] memory _num){
+        uint[10] memory votesArray;
+        for(uint i = 0; i <=  cases[_caseNumber].alternativesSting.length-1; i++){
+            votesArray[i] = caseAlternatives[_caseNumber][cases[_caseNumber].alternativesSting[i]];
+        }
+        
+        return (cases[_caseNumber].title, cases[_caseNumber].deadline, cases[_caseNumber].totalVotes, cases[_caseNumber].openForVoting, cases[_caseNumber].alternativesSting, votesArray);
     }
     function deleteCase(uint _caseNumber) public onlyOwner {
       string memory title = cases[_caseNumber].title;
@@ -77,4 +96,25 @@ contract Cases is Ownable{
    function getCreator(uint _index) public view onlyOwner returns(address){
        return creators[_index];
    }
+   
+   function addAlternatives(uint _caseNumber, string memory _option1) public{
+       caseAlternatives[_caseNumber][_option1] = 0;
+      cases[_caseNumber].alternativesSting.push(_option1);
+      
+   }
+   
+   function Vote (uint _caseNumber, uint _optionVoted) public{
+       
+         caseAlternatives[_caseNumber]["Ikke Stemt"] = SafeMath.sub(caseAlternatives[_caseNumber]["Ikke Stemt"], 1);
+         caseAlternatives[_caseNumber][cases[_caseNumber].alternativesSting[_optionVoted]] = SafeMath.add(caseAlternatives[_caseNumber][cases[_caseNumber].alternativesSting[_optionVoted]], 1);
+           
+     
+   }
+   
+  
+
+
+
+
+   
 }
