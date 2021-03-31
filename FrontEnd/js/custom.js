dc765909cdcc81316cee17c2f942ad9774758f2d
Peshang <peseng88@gmail.comn>
2021-03-31 21:58:32 +0200

    added datepicker to create case

--- FrontEnd/js/custom.js
+++ FrontEnd/js/custom.js
@@ -50,17 +50,3 @@ $(function () {
   
 
 });
-
-
-
- $('#StartDate, #EndDate').datepicker();
-
- $('#EndDate').change(function () {
-   var startDate = document.getElementById('StartDate').value;
-   var endDate = document.getElementById('EndDate').value;
-
-   if (Date.parse(endDate) <= Date.parse(startDate)) {
-     alert('End date should be greater than Start date');
-     document.getElementById('EndDate').value = '';
-   }
- });
