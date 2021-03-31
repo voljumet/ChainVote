3164d3242478a2f20dc4dcb7791d0bce28dbe6ac
Peshang <peseng88@gmail.comn>
2021-03-31 15:30:37 +0200

    added test.html

--- FrontEnd/js/custom.js
+++ FrontEnd/js/custom.js
@@ -46,6 +46,21 @@ $(function () {
     },
   });
 
+  
+  
+
 });
 
 
+
+ $('#StartDate, #EndDate').datepicker();
+
+ $('#EndDate').change(function () {
+   var startDate = document.getElementById('StartDate').value;
+   var endDate = document.getElementById('EndDate').value;
+
+   if (Date.parse(endDate) <= Date.parse(startDate)) {
+     alert('End date should be greater than Start date');
+     document.getElementById('EndDate').value = '';
+   }
+ });
