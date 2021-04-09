e848f972e3ec1179662d2214e7c686e11c75b365
Peshang <peseng88@gmail.comn>
2021-04-09 13:18:13 +0200

    fixed bugs, clean up code

--- BackEnd/contracts/Case.sol
+++ BackEnd/contracts/Case.sol
@@ -53,6 +53,7 @@ contract Case is MultiSig {
                 ))
         );
         emit confirmationE(true);
+        emit getUserE(_region, _userType);
     }
     
     function endVoting(uint256 _caseNumber)public {
