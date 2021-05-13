f06c6dd023758fa1391613daa0aed101102ef1c8
Rune Alexander <alexander.laursen@gmail.com>
2021-05-13 21:02:51 +0200

    abinew

--- FrontEnd/abi.js
+++ FrontEnd/abi.js
@@ -1,18 +1,14 @@
-
 // Testnet
 Moralis.initialize("V72IuyWsaYclkUWnzU7JdLkfSZqyArobvyU4OKOg"); // Application id from moralis.io
 Moralis.serverURL = "https://03i7vk4ziens.moralis.io:2053/server"; //Server url from moralis.io
 
-const jsonCase = require("../Backend/build/contracts/Case.json"); //(with path)
-const jsonProxy = require("../Backend/build/contracts/Proxy.json"); //(with path)
-const jsonMulti = require("../Backend/build/contracts/MultiSig.json"); //(with path)
 // Local
 //Moralis.initialize("2xY2tmcdYBf3IdqY5Yuo74fSEyxigYSADL9Ywtrj"); // Application id from moralis.io
 //Moralis.serverURL = "https://rnonp7vwlz3d.moralis.io:2053/server"; //Server url from moralis.io
 
 function getAbi() {
     return new Promise((res) => {
-        $.getJSON(jsonCase, ((json) => {
+        $.getJSON("../Backend/build/contracts/Case.json", ((json) => {
             res(json.abi);
         }))
     })
@@ -20,7 +16,7 @@ function getAbi() {
 
 function getProxyAbi() {
     return new Promise((res) => {
-        $.getJSON(jsonProxy, ((json) => {
+        $.getJSON("../Backend/build/contracts/Proxy.json", ((json) => {
             res(json.abi);
         }))
     })
@@ -28,7 +24,7 @@ function getProxyAbi() {
 
 function getMultiAbi() {
     return new Promise((res) => {
-        $.getJSON(jsonMulti, ((json) => {
+        $.getJSON("../Backend/build/contracts/MultiSig.json", ((json) => {
             res(json.abi);
         }))
     })
